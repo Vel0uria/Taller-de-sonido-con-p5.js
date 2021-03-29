7da9dce84a6fc956ed0dadbe61a17cb48e8ed5f9
VelOuria <sofleos@gmail.com>
2021-03-29 14:11:04 -0600

    clase 3

--- clase_3/sketch.js
+++ clase_3/sketch.js
@@ -1,35 +1,38 @@
 let voz;
-let sliderFrecuencia;
-let fft;
 let envolvente;
 let filtro;
 let angulo;
 let aumentoAngulo;
+let cir;
+let rects = [];
+const numRects = 10;
 
 function setup() {
   createCanvas(400, 400);
-  // Objeto necesario para el analisis de la onda
-  fft = new p5.FFT();
-  // Objeto para controlar el oscilador
-  voz = new p5.Oscillator();
-  // Objeto para controlar la envolvente
-  envolvente = new p5.Envelope(0.2, 1, 0.2, 0.3, 0.3, 0);
-  // Objeto para controlar el filtro
-  filtro = new p5.Filter();
 
+  voz = new p5.Oscillator();
+
+  envolvente = new p5.Envelope(0.2, 1, 0.2, 0.3, 0.3, 0);
+
+  filtro = new p5.LowPass();
+
+  for (i = 0; i < numRects; i++) {
+    r = new rectObj(
+      random(width),
+      random(height),
+      random(10, 50),
+      random(10, 50)
+    ); // generate a rectObj
+    rects.push(r); //add it to the array.
+  }
+
+  cir = new circleObj(20); // create a new circle object
   // sliderFrecuencia = createSlider(0,1,0.05,0.0001);
 
   // Desconexion del oscilador con el nodo principal de audio
   voz.disconnect();
   // Conexion del oscilador con el nodo de la entrada del filtro
   voz.connect(filtro);
-
-  // Angulo sirve para variar la senoidal que controla la velocidad de crecimiento del circulo central. Tambien ayuda a variar la resonancia del elemento
-  angulo = 0;
-  // aumentoAngulo controla la velocidad de variacion en la senoidal
-  aumentoAngulo = 0.05;
-  // Esta funcion imprime los comandos en consola
-  // instrucciones();
 }
 
 function draw() {
@@ -37,44 +40,15 @@ function draw() {
   // El formato de color se escribe como
   // background(color R, color G, color B, transparecia)
   // Por defecto todos los valores van de 0 a 255
+
   background(0, 0, 0, 20);
-  let corte = logConv(mouseX, 0, width, 20, 20000);
-  let sF = logConv(mouseY, 0, height, 20, 20000);
 
-  // let sF = logConv(sliderFrecuencia.value(),0,1,20,20000);
-
-  voz.freq(sF);
-  filtro.freq(corte);
-  let r = 100 * sin(angulo);
-  r = map(r, -100, 100, 0.001, 10);
-  filtro.res(r);
-  let ancho = map(r, 0.001, 10, 50, 300);
-  stroke(55, 20, 200);
-  circle(width / 2, height / 2, ancho);
-
-  angulo = angulo + aumentoAngulo;
-
-  if (angulo > 2 * PI) {
-    angulo = 0;
+  for (i = 0; i < numRects; i++) {
+    rects[i].disp();
+    rects[i].collide(cir); //collide against the circle object
   }
 
-  let onda = fft.waveform();
-  beginShape();
-  strokeWeight(1);
-  stroke(255, 0, 0);
-  noFill();
-  for (var i = 0; i < onda.length; i++) {
-    let amp = onda[i];
-    let x = map(i, 0, onda.length, 0, width);
-    let y = map(amp, -1, 1, -height / 2, height / 2);
-    y = y + height / 2;
-
-    // circle(x,y,20);
-
-    vertex(x, y);
-    // point(x, y);
-  }
-  endShape();
+  cir.disp(mouseX, mouseY); //pass the x,y pos in to the circle.
 }
 
 function keyPressed() {
@@ -91,7 +65,11 @@ function keyPressed() {
   if (key == "g") {
     envolvente.triggerAttack(filtro);
   }
-
+  if (key == "r") {
+    for (let i = 0; i < rects.length; i++) {
+      rects[i].setNota(40);
+    }
+  }
   if (key == "1") {
     voz.setType("sine");
   }
@@ -121,9 +99,63 @@ function logConv(posicion, min, max, minLog, maxLog) {
 
   return Math.exp(minv + scale * (posicion - minp));
 }
+function rectObj(x, y, w, h) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.color = color(random(255), random(255), random(255));
+  this.hit = false;
+  this.nota = int(random(40, 76));
+  this.collide = function(obj) {
+    this.hit = collideRectCircle(
+      this.x,
+      this.y,
+      this.w,
+      this.h,
+      obj.x,
+      obj.y,
+      obj.dia
+    ); //collide the cir object into this rectangle object.
 
+    if (this.hit) {
+      f = midiToFreq(this.nota);
+      voz.freq(f);
+      envolvente.play(filtro);
+    }
+  };
+
+  this.disp = function() {
+    noStroke();
+    fill(this.color);
+    this.x += 3; //move to the right!
+    if (this.x > width) {
+      //loop to the left!
+      this.x = -this.w;
+    }
+    rect(this.x, this.y, this.w, this.h);
+  };
+  this.setNota = function(n) {
+    this.nota = n;
+  };
+}
+
+function circleObj(dia) {
+  this.dia = dia;
+  this.color = color(random(255), random(255), random(255));
+  this.x;
+  this.y;
+
+  this.disp = function(x, y) {
+    this.x = x;
+    this.y = y;
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.dia, this.dia);
+  };
+}
 // function instrucciones() {
-//   print("Interactua con😊;
+//   print("Interactua con;
 //   print("< a > Iniciar el oscilador");
 //   print("< s > Detener el oscilador");
 //   print("< g > Tocar");
